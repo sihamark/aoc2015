0cf50e4fcf0a71faa2934d4af327dc1035da0772
Hans Markwart <hans@rapidrabbit.com>
2019-01-29 20:39:49 +0100

    - added skeleton for day 8 and day 9

---
+++ day8/src/main/kotlin/aoc2015/day8/main.kt
@@ -0,0 +1,14 @@
+package aoc2015.day8
+
+import aoc2015.prepare
+import aoc2015.start
+
+/**
+ * Created by Hans Markwart on 29.01.2019.
+ */
+fun main(args: Array<String>) {
+    prepare(
+            { "" },
+            { "" }
+    ).start(args)
+}
