5c3a1e8b2d6cf000e75cc2eb8bf9c8cfccd0727c
Hans <fanaloce@gmail.com>
2019-02-02 20:18:52 +0100

    - added day 16 part 1

---
+++ day16/src/main/kotlin/aoc2015/day16/main.kt
@@ -0,0 +1,11 @@
+package aoc2015.day16
+
+import aoc2015.prepare
+import aoc2015.start
+
+fun main(args: Array<String>) {
+    prepare(
+            { "sue number ${Day16.findNumberOfSue()} send you the gift" },
+            { "" }
+    ).start(args)
+}
