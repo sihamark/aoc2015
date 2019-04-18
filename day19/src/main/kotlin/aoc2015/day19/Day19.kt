56a574c6520dd87c926639d02bf205db7611ad2c
Hans <fanaloce@gmail.com>
2019-04-18 10:57:08 +0200

    - added solution for day 19 part 2

--- day19/src/main/kotlin/aoc2015/day19/Day19.kt
+++ day19/src/main/kotlin/aoc2015/day19/Day19.kt
@@ -2,29 +2,66 @@ package aoc2015.day19
 
 import kotlinx.coroutines.*
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 
 object Day19 {
 
     private val replacements = rawReplacements.map { Parser.parseReplacement(it) }
 
     fun numberOfDistinctMoleculesAfterOneReplacement() =
-            replacements.flatMap { it.doReplace(medicine) }
+            replacements.flatMap { it.replaceAll(medicine) }
                     .distinct()
                     .size
 
-    fun findFewestStepsToFabricateMedicine(): Int = runBlocking {
+    fun findFewestStepsToFabricateMedicine(): Int {
+        val inverseReplacements = replacements.map { it.inverse() }
+        var count: Int? = null
+
+        while (count == null) {
+            count = tryToReduceMolecule(medicine, inverseReplacements)
+        }
+
+        return count
+    }
+
+    private fun tryToReduceMolecule(molecule: String, replacements: List<Replacement>): Int? {
+        var count = 0
+        var currentMolecule = molecule
+        val sources = replacements.map { it.source }
+
+        while (currentMolecule != "e") {
+            val randomReplacement = replacements.random()
+
+            if (sources.all { !currentMolecule.contains(it) }) {
+                println("can't reduce $currentMolecule any further abort")
+                return null
+            }
+
+            if (currentMolecule.contains(randomReplacement.source)) {
+                currentMolecule = randomReplacement.replaceFirst(currentMolecule)
+                count++
+            }
+        }
+
+        return count
+    }
+
+    private fun fabrication(): Int = runBlocking {
         val counter = AtomicInteger(0)
+        val latest = AtomicReference("")
         val start = System.currentTimeMillis()
+
         val steps = async {
-            println("begin fabrication")
-            fabricate(medicine, counter, "e")
+            println("begin fabrication target is ${medicine.length} long")
+            fabricate(medicine, counter, latest, listOf(), "e")
         }
 
         launch {
             while (isActive) {
                 delay(5_000)
                 val time = System.currentTimeMillis() - start
-                println("did ${counter.get()} replacements in $time ms")
+                val latestValue = latest.get()
+                println("did ${counter.get()} replacements in $time ms latest(${latestValue.length}): $latestValue")
             }
         }
 
@@ -42,23 +79,31 @@ object Day19 {
         steps.await()
     }
 
-    private suspend fun fabricate(target: String, counter: AtomicInteger, current: String, steps: Int = 0): Int {
+    private suspend fun fabricate(target: String, counter: AtomicInteger, latest: AtomicReference<String>, already: List<String>, current: String, steps: Int = 0): Int {
         yield()
 
-        val currentReplacements = replacements.flatMap { it.doReplace(current) }
+        val newAlready = already.toMutableList().apply {
+            add(current)
+        }
+
+        latest.set(current)
+
+        val currentReplacements = replacements.flatMap { it.replaceAll(current) }
                 .distinct()
                 .filter { it.length <= target.length }
+                .filter { !newAlready.contains(it) }
 
         counter.incrementAndGet()
         val newStep = steps + 1
 
         return when {
+            current == target -> steps
             currentReplacements.any { it == target } -> newStep
             currentReplacements.isEmpty() -> Integer.MAX_VALUE
             else -> {
                 var min = Integer.MAX_VALUE
                 for (replacement in currentReplacements) {
-                    val newSteps = fabricate(target, counter, replacement, newStep)
+                    val newSteps = fabricate(target, counter, latest, newAlready, replacement, newStep)
                     if (newSteps < min) {
                         min = newSteps
                     }
@@ -82,9 +127,13 @@ object Day19 {
             val source: String,
             val replacement: String
     ) {
-        fun doReplace(medicine: String) = Regex(source).findAll(medicine)
+        fun replaceFirst(molecule: String) = molecule.replaceFirst(source, replacement)
+
+        fun replaceAll(medicine: String) = Regex(source).findAll(medicine)
                 .map { medicine.replaceRange(it.range, replacement) }
                 .distinct()
                 .toList()
+
+        fun inverse() = Replacement(replacement, source)
     }
 }
