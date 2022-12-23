705af1bd8048da3547b80a0d942b86965946e516
Ptah <hans@rapidrabbit.com>
2022-12-23 22:39:31 +0100

    - fixed sum by deprecations - deleted build gradle content

--- day12/src/main/kotlin/aoc2015/day12/Day12.kt
+++ day12/src/main/kotlin/aoc2015/day12/Day12.kt
@@ -17,19 +17,19 @@ object Day12 {
 
     private fun sum(any: Any): Int = when (any) {
         is Number -> any.toInt()
-        is List<*> -> any.sumBy { sum(it ?: 0) }
-        is Map<*, *> -> any.entries.sumBy { sum(it.key ?: 0) + sum(it.value ?: 0) }
+        is List<*> -> any.sumOf { sum(it ?: 0) }
+        is Map<*, *> -> any.entries.sumOf { sum(it.key ?: 0) + sum(it.value ?: 0) }
         else -> 0
     }
 
     private fun sumNotRed(any: Any): Int = when (any) {
         is Number -> any.toInt()
-        is List<*> -> any.sumBy { sumNotRed(it ?: 0) }
+        is List<*> -> any.sumOf { sumNotRed(it ?: 0) }
         is Map<*, *> -> {
             if (any.values.any { it == "red" }) {
                 0
             } else {
-                any.entries.sumBy { sumNotRed(it.key ?: 0) + sumNotRed(it.value ?: 0) }
+                any.entries.sumOf { sumNotRed(it.key ?: 0) + sumNotRed(it.value ?: 0) }
             }
         }
         else -> 0
